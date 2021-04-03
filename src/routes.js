8ce4eda6a63d9d693917c0baf57f96ad6d12f513
Maximos007 <waslleymax@gmail.com>
2021-04-03 16:27:41 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,79 @@ const profile = {
 	'monthly-budget': 3000,
 	'days-per-week': 5,
 	'hours-per-day': 5,
-	'vacation-per-year': 4
+	'vacation-per-year': 4,
+	'value-hour': 75
 };
 
-// routes.get('/index.html', (req, res) =>  res.redirect('/'));
-routes.get('/', (req, res) => res.render(views + 'index'));
+const jobs = [
+	{
+		id: 1,
+		name: 'Pizzaria Guloso', 
+		'daily-hours': 2, 
+		'total-hours': 1,
+		createdAt: Date.now(),// atribuindo uma nova data
+
+	},
+	{
+		id: 1,
+		name: 'OneTwo Project', 
+		'daily-hours': 3, 
+		'total-hours': 47,
+		createdAt: Date.now(), // atribuindo uma nova data
+	}
+];
+
+function remainingDays(job) {
+	const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed();
+
+	const createDate = new Date(job.createdAt);
+	const dueDay = createDate.getDate() + Number(remainingDays);
+	const  dueDateInMs = createDate.setDate(dueDay);
+
+	const timeDiffInMs = dueDateInMs - Date.now();
+	// Transformar milli em dias 
+	const dayInMs = 1000 * 60 * 60 * 24;
+	const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+
+	//restam x dias 
+	return dayDiff;
+}
+
+routes.get('/', (req, res) => {
+
+	const updatedJobs = jobs.map((job) => {
+		// Ajustes no job
+		const remaining = remainingDays(job);
+		const status = remaining <=0 ? 'done' : 'in progress';
+
+		return {
+			...job,
+			remaining,
+			status,
+			budget: profile['value-hour'] * job['total-hours']
+		};
+	});
+
+	return res.render(views + 'index', { jobs : updatedJobs });
+});
 
 routes.get('/job', (req, res) => res.render(views + 'job'));
+
+// rota do formulário
+routes.post('/job', (req, res) => {
+	// req.body = { name: 'nodeJS', 'daily-hours': '3.1', 'total-hours': '3'}
+	const lastId = jobs[jobs.length - 1]?.id || 1;
+
+	jobs.push({
+		id: lastId + 1,
+		name: req.body.name, 
+		'daily-hours': req.body['daily-hours'], 
+		'total-hours': req.body['total-hours'],
+		createdAt: Date.now() // atribuindo uma nova data 
+	});
+	return res.redirect('/');
+});  
+
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
 routes.get('/profile', (req, res) => res.render(views + 'profile', { profile}));
 
--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const server = express();
 const routes = require('./routes');
 
+// usando a template engine
 server.set('view engine', 'ejs');
 
 // habilitar arquivos statics
 server.use(express.static('public'));
 
+// usar o req.body -> liberação do corpo da requisição
+server.use(express.urlencoded({ extended: true }));
+
+// routes
 server.use(routes);
 
 server.listen(3333, () => console.log('server is running'));
